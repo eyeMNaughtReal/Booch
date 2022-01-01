15338d587a2ba1086d6aade46758b0b81210644c
Eyemnaughtreal <mykid8yours@gmail.com>
2022-01-01 00:23:51 -0500

    First commit of Kombucha App

--- Booch/BoochApp.swift
+++ Booch/BoochApp.swift
@@ -12,6 +12,7 @@ struct BoochApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                
         }
     }
 }
--- Booch/ContentView.swift
+++ Booch/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
+let lighGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+let darkGreyColor = Color (red: 38.0/255.0, green: 37.0/255.0, blue: 37.0/255.0, opacity: 1.0)
+let fontLightGreyColor = Color (red: 228.0/255.0, green: 228.0/255.0, blue: 228.0/255.0, opacity: 1.0)
+
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        VStack {
+            Text("Booch")
+                .fontWeight(.bold)
+                .foregroundColor(fontLightGreyColor)
+                .padding(.bottom, 60)
+                .font(.largeTitle)
+                
+            WelcomeText()
+            UserImage()
+            TextField("Username" , text: $username)
+                .padding()
+                .background(lighGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField("Password" , text: $password)
+                .padding()
+                .background(lighGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Text("LOGIN")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 250, height: 60)
+                .background(Color.green)
+                .cornerRadius(15.0)
+            Button(action: {print("Button tapped")})
+            {}
+        }
+    
+        .padding()
+        .background(darkGreyColor)
     }
-}
+        
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome!")
+            .font(.title)
+            .fontWeight(.semibold)
+            .foregroundColor(fontLightGreyColor)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image("userImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+        }
     }
 }
