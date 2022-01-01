423151372c35f42ac09c9854ad1e99122011697d
Eyemnaughtreal <mykid8yours@gmail.com>
2021-12-31 22:37:12 -0500

    Initial Commit

---
+++ Booch/BoochApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoochApp.swift
+//  Booch
+//
+//  Created by Mark Conley on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct BoochApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
