cb9e7dd84f58f58937b5961975993d82ad129e87
eyemnaughtreal <mykid8yours@gmail.com>
2022-07-06 13:25:07 -0400

    Updated names on the different views

--- Booch/BrewView.swift
+++ Booch/BrewView.swift
@@ -11,7 +11,7 @@ struct BrewView: View {
     var body: some View {
         
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Brew")
             
         }
     }
